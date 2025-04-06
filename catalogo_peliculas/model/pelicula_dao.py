d531bb60f8f1f1898dbe84569a19f469561397e4
JelsinPalomino <jstpalomino@hotmail.com>
2025-04-05 19:44:18 -0500

    [code] Add the methods crear_table and borrar_table in model\pelicula_dao.py

---
+++ catalogo_peliculas/model/pelicula_dao.py
@@ -0,0 +1,24 @@
+from .conexion_db import ConexionDB
+
+def crear_tabla():
+    conexion = ConexionDB()
+
+    sql = '''
+    CREATE TABLE peliculas(
+        id_pelicula INTEGER,
+        nombre VARCHAR(100),
+        duracion VARCHAR(10),
+        genero VARCHAR(100),
+        PRIMARY KEY(id_pelicula AUTOINCREMENT)
+    )'''
+
+    conexion.cursor.execute(sql)
+    conexion.cerrar()
+
+def borrar_table():
+    conexion = ConexionDB()
+
+    sql = 'DROP TABLE peliculas'
+
+    conexion.cursor.execute(sql)
+    conexion.cerrar()
